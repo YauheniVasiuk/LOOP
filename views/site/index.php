77e8826c22b4a858ae6489215b3ab57daa52475a
YauheniVasiuk <95546369+YauheniVasiuk@users.noreply.github.com>
2022-05-06 18:40:36 +0300

    fifth point

--- views/site/index.php
+++ views/site/index.php
@@ -244,8 +244,83 @@
       </section>
 
       <footer class="footer">
-         <div class="footer_top">Footer top</div>
-         <div class="footer_bot">Footer bottom</div>
+         <div class="footer_top">
+            <div class="container">
+
+               <div class="footer_list">
+                  <div class="footer_item">
+
+                     <div class="footer_logo">
+                        <a href="#"><span class="ico_logo"><span class="title_logo">THE&nbspLOOP</span></span></a>
+                     </div>
+
+                     <p>Интернет-магазин the LOOP наиболее удобный способ покупать мужскую одежду и обувь в Республике Беларусь. </p>
+
+                  </div>
+
+                  <div class="footer_item">
+
+                     <h4>Основные ссылки</h4>
+
+                     <ul class="footer_links">
+                        <li><a href=" #">О компании</a></li>
+                        <li><a href="#">Каталог</a></li>
+                        <li><a href="#">Доставка</a></li>
+                        <li><a href="#">Оплата</a></li>
+                     </ul>
+
+                  </div>
+                  <div class="footer_item">
+
+                     <h4>Категории</h4>
+
+                     <ul class="footer_links">
+                        <li><a href="#">Мужская одежда</a></li>
+                        <li><a href="#">Женская одежда</a></li>
+                        <li><a href="#">Детская одежда</a></li>
+                        <li><a href="#">Для животных</a></li>
+                     </ul>
+
+                  </div>
+                  <div class="footer_item">
+
+                     <h4>ПОЛЕЗНЫЕ ССЫЛКИ</h4>
+
+                     <ul class="footer_links">
+                        <li><a href="#">Таблица размеров</a></li>
+                        <li><a href="#">Блог о моде</a></li>
+                        <li><a href="#">Наша миссия</a></li>
+                     </ul>
+
+                  </div>
+                  <div class="footer_item">
+
+                     <h4>СОЦ СЕТИ</h4>
+
+                     <ul class="footer_links">
+                        <li><span class="ico_vk"></span><a href="#">VK.com</a></li>
+                        <li><span class="ico_Facebook"></span><a href="#">Facebook</a></li>
+                        <li><span class="ico_Instagram"></span><a href="#">Instagramm</a></li>
+                     </ul>
+
+                  </div>
+               </div>
+
+            </div>
+         </div>
+         <div class="footer_bot">
+            <div class="container">
+
+               <div class="footer_bot_wrapper">
+
+                  <div class="footer_bot_copyrights">Copyright &copy; 2019. Все права защищены</div>
+
+                  <div class="footer_bot_private">Политика приватности</div>
+
+               </div>
+
+            </div>
+         </div>
       </footer>
 
 
